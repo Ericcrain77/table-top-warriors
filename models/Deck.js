6934b3026128721d507f35570626ee63be510286
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-11-16 21:27:52 -0500

    created Deck model and relationship with user model in index file

---
+++ models/Deck.js
@@ -0,0 +1,39 @@
+//require sequelize model and datatypes for creating model
+const {Model, DataTypes} = require('sequelize');
+//connect to the database
+const sequelize = require('../config/connection');
+
+//create Deck model
+class Deck extends Model{}
+
+//initialize the Deck model and define the characteristics it should contain
+Deck.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        deck_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        deck_owner: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'deck'
+    }
+);
+
+module.exports = Deck;
--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const User = require("./User");
-const Game = require('./Game');
+const Game = require("./Game");
 
 //TABLE RELATIONSHIPS WILL GO HERE
 User.hasMany(Game, {
     foreignKey: 'host_id'
-})
+});
+
+User.hasMany(Deck, {
+    foreignKey: 'deck_owner'
+});
 
 module.exports = {User, Game};
