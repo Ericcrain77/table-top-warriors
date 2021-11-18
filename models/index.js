7d0416375193f99f2f5aaf3d58d0bbc810fb134b
JBanana <johnandrewharris3@gmail.com>
2021-11-17 19:18:43 -0600

    table relationships

--- models/index.js
+++ models/index.js
@@ -7,8 +7,16 @@ User.hasMany(Game, {
     foreignKey: 'host_id'
 });
 
+Game.belongsTo(User, {
+    foreignKey: "host_id"
+});
+
 User.hasMany(Deck, {
     foreignKey: 'deck_owner'
+});
+
+Deck.belongsTo(User, {
+    foreignKey: "deck_owner"
 });
 
 module.exports = {User, Game, Deck};
