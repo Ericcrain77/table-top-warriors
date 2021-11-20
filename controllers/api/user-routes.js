56850f1e7f60a9e9457f0be13d5f5fb95aea85ed
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-11-19 21:43:36 -0500

    created route to get all users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {User} = require("../../models");
+const {User, Deck, Card} = require("../../models");
 //login authentication will go here
 
 //route to get all users
@@ -14,5 +14,43 @@ router.get("/", (req, res) => {
     });
 });
 
+//route to get a single user
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Deck,
+                attributes: [
+                    'id',
+                    'deck_name',
+                    'game_id'
+                ],
+                include: {
+                    model: Card,
+                    attributes: [
+                        'id',
+                        'card_name',
+                        'game_id'
+                    ]
+                }
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
