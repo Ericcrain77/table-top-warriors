2d93be8126bd51c6663fddee5f2d0d18eb2e1b1c
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-11-20 09:25:55 -0500

    created route to get a single card

--- controllers/api/card-routes.js
+++ controllers/api/card-routes.js
@@ -23,4 +23,34 @@ router.get('/', (req, res) => {
     });
 });
 
+//route to get a single card
+router.get('/:id', (req, res) => {
+    Card.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Deck,
+                attributes: ['deck_name'],
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbCardData => {
+        if (!dbCardData) {
+            res.status(404).json({ message: 'No card found with this id' });
+            return;
+        }
+        res.json(dbCardData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
