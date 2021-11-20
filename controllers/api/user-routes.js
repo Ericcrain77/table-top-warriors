cc06bb8188d32252fa5616a05fb1ea0a4ff3edf3
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-11-20 00:10:26 -0500

    created routes to login and to logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -76,4 +76,44 @@ router.post('/', (req, res) => {
     });
 });
 
+//route to login 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+        
+        const validPw = dbUserData.checkPassword(req.body.password);
+        if (!validPw) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are logged in!' });
+        });
+    });
+});
+
+//route to logout
+router.post('/logout', withAuth, (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+})
+
 module.exports = router;
