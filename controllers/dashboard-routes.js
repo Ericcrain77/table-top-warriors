ee9c1aef711885d738263c4d9347519c69cda3c5
JBanana <johnandrewharris3@gmail.com>
2021-11-22 14:48:54 -0600

    small changes to home and dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const {User, Game, Deck} = require("../models");
+const {User, Deck} = require("../models");
 const withAuth = require("../utils/auth");
 
 
